2ce361245de6a33ed02cba31e04f3acf6f07cabf
baegteun <baegteun@gmail.com>
2023-01-10 15:04:34 +0900

    :memo: :: 코드 주석화

--- Sources/Miniature/Miniature.swift
+++ Sources/Miniature/Miniature.swift
@@ -1,12 +1,19 @@
 import Combine
 import Foundation
 
+/// The `Miniature` struct is designed to handle loading data from both local and remote sources,
+/// as well as caching it locally.
 public struct Miniature<T> {
+    /// `onLocal` is a closure that returns data from the local cache.
     var onLocal: (() -> T?)
+    /// `onRemote` is an optional closure that returns data from a remote source as a publisher
     var onRemote: (() -> AnyPublisher<T, Error>)?
+    /// `onAsyncRemote` is an optional closure that returns data from a remote source as an async task
     var onAsyncRemote: (() async throws -> T)?
+    /// `refreshLocal` is a closure that updates the local cache with new data.
     var refreshLocal: ((T) -> Void)
 
+    /// Initializer that accepts onRemote closure, returns a publisher emitting data from a remote source.
     public init(
         onLocal: @escaping () -> T?,
         onRemote: @escaping () -> AnyPublisher<T, Error>,
@@ -18,6 +25,7 @@ public struct Miniature<T> {
         self.refreshLocal = refreshLocal
     }
 
+    /// Initializer that accepts onAsyncRemote closure, returns an async task that throws error
     public init(
         onLocal: @escaping () -> T?,
         onRemote: @escaping () async throws -> T,
@@ -29,6 +37,9 @@ public struct Miniature<T> {
         self.refreshLocal = refreshLocal
     }
 
+    /// Method to load data from a remote source using the `onRemote` closure and calls the provided action closure
+    /// with the appropriate `MiniatureStatus` enum case.
+    /// This method returns an object that conforms to AnyCancellable protocol, which can be used to cancel the task.
     public func publish(
         _ action: @escaping (MiniatureStatus<T>) -> Void
     ) -> AnyCancellable {
@@ -47,6 +58,8 @@ public struct Miniature<T> {
         return cancellable
     }
 
+    /// Method to load data from a remote source using the `onAsyncRemote` closure and calls the provided action closure
+    /// with the appropriate `MiniatureStatus` enum case.
     public func asyncPublish(
         _ action: @escaping (MiniatureStatus<T>) -> Void
     ) async {
--- Sources/Miniature/MiniatureStatus.swift
+++ Sources/Miniature/MiniatureStatus.swift
@@ -1,10 +1,12 @@
 import Foundation
 
+/// The `MiniatureStatus` Enum defines the possible states of data loading: loading, completed, and error.
 public enum MiniatureStatus<T> {
     case loading(T?)
     case error(Error)
     case completed(T)
 
+    /// Helper function to handle the enum case
     public func action<V>(
         onLoading: (T?) -> V,
         onCompleted: (T) -> V,
